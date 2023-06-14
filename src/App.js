7a7e00fd9625ac5e3f314d877e0493a5ceab6e6e
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-06-14 15:58:23 -0300

    req 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       Hello, TrybeWallet!
       <Route exact path="/" component={ Login } />
-
+      <Route path="/carteira" component={ Wallet } />
     </div>
   );
 }
